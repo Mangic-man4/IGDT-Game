ccb610c6d2ba76075437587495082b7ce30cf91a
Bbyface <horrortuuletin@gmail.com>
2023-11-06 16:38:46 +0200

    PlayerController
    Added left and right movement, jumping controls and checker for if character is grounded.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D player;
     public float characterSpeed = 5.0f;
-    public float characterJump = 2.0f;
+    public float characterJump = 200f;
     public float gravityScale = 1f;
     public Camera mainCamera;
-    bool isGrounded = false;
+    private float isJumping = 0f;
+    Vector2 cameraPos;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
 
     void Start()
     {
-
+        player = GetComponent<Rigidbody2D>();
         //Lukitsee kameran pelaajan hahmoon
         if (mainCamera)
         {
-            cameraPos = mainCamera.transform.position
+            cameraPos = mainCamera.transform.position;
         }
         
     }
@@ -27,6 +33,38 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        if ((Input.GetKey(KeyCode.A)
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isJumping = Input.GetAxis("Horizontal");
+
+        if (isJumping > 0f) 
+        {
+            player.velocity = new Vector2(isJumping * characterSpeed, player.velocity.y);
+        }
+        else if (isJumping < 0f) 
+        {
+            player.velocity = new Vector2(isJumping * characterSpeed, player.velocity.y);
+        }
+        else 
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        //Moves character left if "A" key is pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector2.left * Time.deltaTime * characterSpeed);
+        }
+
+        //Moves character right if "D" key is pressed
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            this.transform.Translate(Vector2.right * Time.deltaTime * characterSpeed);
+        }
+
+        //Makes character jump if Spacebar is pressed
+        if (Input.GetKey(KeyCode.Space) && isTouchingGround)
+        {
+                player.velocity = new Vector2(player.velocity.x, characterJump);
+        }
     }
 }
