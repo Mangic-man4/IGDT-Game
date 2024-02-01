457f204343def15d46a829b5a3fff822e7d76691
Zapult <127941539+Zapult@users.noreply.github.com>
2024-02-01 11:21:18 +0200

    Spike visibility & level selector
    Fixed visibility with the upper spikes and added a level selector

---
+++ Assets/Scripts/Button scripts/LevelSelector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    public Text levelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = level.ToString();
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+}
