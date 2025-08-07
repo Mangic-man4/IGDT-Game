2ef8d6fc7c1083a144301be1344bd8d4b2b659b6
Haaksi <126588878+Haaksi@users.noreply.github.com>
2025-08-07 11:52:20 +0300

    Added modified Turret script with working animations

--- Assets/Scripts/Enemy/LaserTurret.cs
+++ Assets/Scripts/Enemy/LaserTurret.cs
@@ -1,22 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 
 public class LaserTurret : MonoBehaviour
 {
     [Header("References")]
     public Transform firePoint;              // Still on root
-    public SpriteRenderer laserSpriteRenderer;   // On child object (LaserBeam)
+    public float maxDistance = 100f;
+    public LayerMask layerMask;
+
+    private SpriteRenderer laserRenderer;   // On child object (LaserBeam)
 
     private Vector3 originalPosition;
     private Quaternion originalRotation;
+
+    // For laser
+    private float distance;
+    private Vector3 direction;
 
     void Awake()
     {
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+    }
+
+    private void Start()
+    {
+        if (firePoint != null)
+            laserRenderer = firePoint.GetComponent<SpriteRenderer>();
+
+        GetPlacementDirection();
     }
 
     void Update()
@@ -26,67 +38,53 @@ public class LaserTurret : MonoBehaviour
 
     void ShootLaser()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right, 100f);
-        Vector3 endPoint = hitInfo ? hitInfo.point : firePoint.position + firePoint.right * 100f;
+        if (firePoint == null || laserRenderer == null) return;
 
-        Debug.DrawRay(firePoint.position, firePoint.right, Color.red);
- 
-        float laserLength = Vector2.Distance(firePoint.position, endPoint);
- 
-        Debug.Log($"Laser Length: {laserLength}");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxDistance, layerMask);
+        distance = hit.collider ? hit.distance : maxDistance;
 
-        laserSpriteRenderer.size = new Vector2(laserLength, laserSpriteRenderer.size.y);
-        laserSpriteRenderer.transform.position = firePoint.position;
-        laserSpriteRenderer.transform.rotation = firePoint.rotation; // just in case rotation matters
+        Vector2 size = laserRenderer.size;
+        size.x = distance;
+        laserRenderer.size = size;
+
+        // Position the laser midpoint in world space
+        firePoint.position = transform.position + (direction * distance * 0.5f);
+
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            var powerUps = hit.collider.GetComponent<PlayerPowerUps>();
+            var playerController = hit.collider.GetComponent<PlayerController>();
+
+            if (powerUps != null && playerController != null)
+            {
+                if (powerUps.IsShieldActive() && powerUps.IsEnemyProtectionEnabled())
+                {
+                    if (powerUps.TryUseShield())
+                    {
+                        Debug.Log("Laser hit absorbed by shield.");
+                        return;
+                    }
+                }
+
+                if (!powerUps.IsInvincible())
+                {
+                    playerController.Die();
+                    Debug.Log("Player has died! Triggered by LaserTurret.");
+                }
+            }
+        }
     }
 
- //   void ShootLaser()
- //   {
- //       if (laserSpriteRenderer== null) return;
- //
- //       RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
- //       Vector3 endPoint;
- //
- //       Debug.DrawRay(firePoint.position, firePoint.right * 5f, Color.red);
- //
- //       if (hitInfo)
- //       {
- //           endPoint = hitInfo.point;
- //           
- //           if (hitInfo.collider.CompareTag("Player") &&
- //               hitInfo.collider.TryGetComponent<PlayerPowerUps>(out var powerUps) &&
- //               hitInfo.collider.TryGetComponent<PlayerController>(out var playerController))
- //           {
- //               if (powerUps.IsShieldActive() && powerUps.IsEnemyProtectionEnabled())
- //               {
- //                   if (powerUps.TryUseShield())
- //                   {
- //                       Debug.Log("Laser hit absorbed by shield.");
- //                       return;
- //                   }
- //               }
- //
- //               if (!powerUps.IsInvincible())
- //               {
- //                   playerController.Die();
- //                   Debug.Log("Player has died! Triggered by LaserTurret.");
- //               }
- //           
- //
- //       }
- //       else
- //       {
- //           endPoint = firePoint.position + firePoint.right * 100f;
- //       }
- //
- //       float laserLength = Vector2.Distance(firePoint.position, endPoint);
- //
- //       laserSpriteRenderer.size = new Vector2(laserLength, laserSpriteRenderer.size.y);
- //       laserSpriteRenderer.transform.position = firePoint.position;
- //   }
     public void RespawnTurret()
     {
         transform.SetPositionAndRotation(originalPosition, originalRotation);
         gameObject.SetActive(true);
+    }
+
+    void GetPlacementDirection()
+    {
+        // Determine direction based on scale.x (lossyScale.x)
+        float facing = Mathf.Sign(transform.lossyScale.x);
+        direction = new Vector2(facing, 0f);
     }
 }
